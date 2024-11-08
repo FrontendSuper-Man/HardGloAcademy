48cecc23b5a8fb5927eaeabd14cd42500bc5c295
Худойкулов Суннат <khudoykulov07@bk.ru>
2024-11-08 23:09:40 +0500

    Сделал 1 задание

--- script-hard.js
+++ script-hard.js
@@ -1,23 +1,34 @@
-let num = 266219;
-console.log(num);
+// let lang = 'ru'
+let lang = 'eng'
 
-let numString = num.toString()
+if (lang === 'ru') {
+	console.log(`
+Понедельник
 
+Вторник
 
-let result = 1;
-for (const i of numString) {
-	result *= +i
+Среда
+
+Четверг
+
+Пятница
+
+Суббота
+
+Воскресенье`);
+} else if (lang === 'eng') {
+	console.log(`
+Monday
+
+Tuesday
+
+Wednesday
+
+Thursday
+
+Friday
+
+Saturday
+
+Sunday`);
 }
-console.log(result);
-
-
-let numResult = result ** 3
-console.log(numResult);
-
-
-let r = numResult.toString().substring(0, 2)
-console.log(r);
-
-
-
-
