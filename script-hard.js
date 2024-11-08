dafbfe55cc27b13b84ec7ba60f389116d5c13c87
Худойкулов Суннат <khudoykulov07@bk.ru>
2024-11-08 21:11:22 +0500

    Все

--- script-hard.js
+++ script-hard.js
@@ -1,21 +1,3 @@
-let num = 266219;
-let numString = num.toString()
-
-
-let result = 1;
-for (const i of numString) {
-	result *= +i
-}
-console.log(result);
-
-
-let numResult = result ** 3
-console.log(numResult);
-
-
-let r = numResult.toString().substring(0, 2)
-console.log(r);
-
 
 
 
